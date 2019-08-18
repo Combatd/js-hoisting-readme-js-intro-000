35a9a200a7973b34039181e548f01dcd4da3e963
hilarious-programmer-2558 <combatdstar@gmail.com>
2019-08-18 16:23:54 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -24,8 +24,7 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
-
+  
   function sayMy() { // inner functions auto hoisted to top
     var name;
     name = "Kristin";
